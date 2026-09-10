f4e15748b95afe587d4f18b980ba85653f9e44a6
jskri <jskri@proton.me>
2026-09-10 17:09:44 +0200

    ci: add js new tests

--- check-proofs-run-tests.sh
+++ check-proofs-run-tests.sh
@@ -1,21 +1,44 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo -n "1/4. Making rocq user the file owner... "
+echo -n "1/8. Making rocq user the file owner... "
 sudo chown -R 1000:1000 . # rocq user
-trap 'echo -n "4/4. Restoring file ownership... " && \
+trap 'echo -n "8/8. Restoring file ownership... " && \
   sudo chown -R "$(id -u)":"$(id -g)" . && \
   echo "done."' EXIT
 echo "done."
 echo ""
 
-docker run --rm -v "$PWD":/workspace -w /workspace formal-tetris-ci \
+docker run --rm -v "$PWD":/workspace -w /workspace formal-tetris-ci:latest \
   bash -c "\
-  echo '2/4. Checking proofs...' && \
+  echo '2/8. Rocq: checking proofs...' && \
   dune build --display=progress --action-stdout-on-success=swallow --action-stderr-on-success=must-be-empty && \
-  echo 'Checking proof: done.' && \
+  echo 'Rocq: checking proofs: OK.' && \
   echo '' && \
-  echo '3/4. Running JS tests.' && \
-  cd js && npm install && npm test && \
-  echo 'Running JS tests: done.' && \
-  echo ''"
+  \
+  cd js && \
+  \
+  echo '3/8. JS: installing dependencies...' && \
+  npm ci && \
+  echo '3/8. JS: installing dependencies: OK.' \
+  echo '' && \
+  \
+  echo '4/8. JS: checking format...' && \
+  npm run format:check && \
+  echo '4/8. JS: checking format: OK.' \
+  echo '' && \
+  \
+  echo '5/8. JS: linting...' && \
+  npm run lint && \
+  echo '5/8. JS: linting: OK.' \
+  echo '' && \
+  \
+  echo '6/8. JS: checking syntax...' && \
+  npm run syntax-check && \
+  echo '6/8. JS: checking syntax: OK.' \
+  echo '' && \
+  \
+  echo '7/8. JS: testing...' && \
+  npm install && npm test && \
+  echo '7/8. JS: testing: OK.'"
+
